4772dba522f229e83051d9716fdb46fbc407674f
이정훈 <eu4ng97@gmail.com>
2025-04-20 17:53:28 +0900

    Feat: WidgetManagerComponent 클래스 작성

---
+++ Source/WidgetManager/Private/Components/WidgetManagerComponent.cpp
@@ -0,0 +1,123 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/WidgetManagerComponent.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Logging.h"
+
+UUserWidget* UWidgetManagerComponent::GetOrCreateWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (WidgetClass ==  nullptr) return nullptr;
+
+    return WidgetMap.Contains(WidgetClass) ? WidgetMap[WidgetClass].Get() : RegisterWidget(WidgetClass);
+}
+
+void UWidgetManagerComponent::ShowWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (UUserWidget* Widget = GetOrCreateWidget(WidgetClass))
+    {
+        if (Widget->IsInViewport())
+        {
+            LOG_ACTOR_COMPONENT(Warning, TEXT("%s is already in viewport."), *WidgetClass->GetName())
+        }
+        else
+        {
+            Widget->AddToViewport();
+
+            LOG_ACTOR_COMPONENT(Log, TEXT("%s is added to viewport."), *WidgetClass->GetName())
+        }
+    }
+}
+
+void UWidgetManagerComponent::HideWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    // 위젯 비활성화
+    if (UUserWidget* Widget = GetOrCreateWidget(WidgetClass))
+    {
+        if (!Widget->IsInViewport())
+        {
+            LOG_ACTOR_COMPONENT(Warning, TEXT("%s is already not in viewport."), *WidgetClass->GetName())
+        }
+        else
+        {
+            Widget->RemoveFromParent();
+
+            LOG_ACTOR_COMPONENT(Log, TEXT("%s is removed from viewport."), *WidgetClass->GetName())
+        }
+    }
+}
+
+void UWidgetManagerComponent::ToggleWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    if (UUserWidget* Widget = GetOrCreateWidget(WidgetClass))
+    {
+        if (Widget->IsInViewport())
+        {
+            HideWidget(WidgetClass);
+        }
+        else
+        {
+            ShowWidget(WidgetClass);
+        }
+    }
+}
+
+APlayerController* UWidgetManagerComponent::GetPlayerController() const
+{
+    UClass* OwnerClass = GetOwner()->GetClass();
+    APlayerController* PlayerController = nullptr;
+
+    if (OwnerClass->IsChildOf(APlayerController::StaticClass()))
+    {
+        PlayerController = Cast<APlayerController>(GetOwner());
+    }
+    else if (OwnerClass->IsChildOf(APawn::StaticClass()))
+    {
+        APawn* OwningPawn = Cast<APawn>(GetOwner());
+        PlayerController = Cast<APlayerController>(OwningPawn->Controller);
+    }
+
+    return PlayerController;
+}
+
+UUserWidget* UWidgetManagerComponent::RegisterWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    // 유효성 검사
+    if (WidgetClass == nullptr) return nullptr;
+
+    // 중복 호출 방지
+    if (WidgetMap.Contains(WidgetClass))
+    {
+        LOG_ACTOR_COMPONENT(Error, TEXT("%s is already registered."), *WidgetClass->GetName())
+    }
+    else
+    {
+        // 위젯 생성 및 등록
+        UUserWidget* Widget = CreateWidget<UUserWidget>(GetPlayerController(), WidgetClass);
+        WidgetMap.Emplace(WidgetClass, Widget);
+
+        LOG_ACTOR_COMPONENT(Log, TEXT("%s is registered"), *WidgetClass->GetName())
+    }
+
+    return WidgetMap[WidgetClass];
+}
+
+void UWidgetManagerComponent::UnRegisterWidget(TSubclassOf<UUserWidget> WidgetClass)
+{
+    // 유효성 검사
+    if (WidgetClass == nullptr) return;
+
+    // 중복 호출 방지
+    if (!WidgetMap.Contains(WidgetClass))
+    {
+        LOG_ACTOR_COMPONENT(Error, TEXT("%s is already unregistered."), *WidgetClass->GetName())
+        return;
+    }
+
+    // 위젯 파괴 및 등록 해제
+    HideWidget(WidgetClass);
+    WidgetMap.Remove(WidgetClass);
+
+    LOG_ACTOR_COMPONENT(Log, TEXT("%s is unregistered"), *WidgetClass->GetName())
+}
---
+++ Source/WidgetManager/Public/Components/WidgetManagerComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WidgetManagerComponent.generated.h"
+
+/**
+ * 위젯 풀링 기능을 지원하는 액터 컴포넌트로,
+ * PlayerController 혹은 Pawn 클래스에서만 사용 가능합니다.
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class WIDGETMANAGER_API UWidgetManagerComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "State")
+    TMap<TSubclassOf<UUserWidget>, TObjectPtr<UUserWidget>> WidgetMap;
+
+public:
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE UUserWidget* GetOrCreateWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ShowWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void HideWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ToggleWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+protected:
+    UFUNCTION(BlueprintPure)
+    APlayerController* GetPlayerController() const;
+
+    UFUNCTION(BlueprintCallable)
+    virtual UUserWidget* RegisterWidget(TSubclassOf<UUserWidget> WidgetClass);
+
+    UFUNCTION(BlueprintCallable)
+    virtual void UnRegisterWidget(TSubclassOf<UUserWidget> WidgetClass);
+};
