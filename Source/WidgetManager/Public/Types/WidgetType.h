7a8ae429d2bd000aa214cd2d5dde1dff0e31034b
이정훈 <eu4ng97@gmail.com>
2025-04-20 21:40:28 +0900

    Feat: WidgetType 열거형 작성

---
+++ Source/WidgetManager/Public/Types/WidgetType.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetType.generated.h"
+
+UENUM(BlueprintType)
+enum class EWidgetType : uint8
+{
+    HUD, // Don't show mouse cursor.
+    Main, // Mouse cursor should be shown and all sub widgets must be closed when main widget opened.
+    Sub // Mouse cursor should be shown.
+};
