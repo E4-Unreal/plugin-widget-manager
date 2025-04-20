3be3dc3792789f88e771ed95952aabb5e5e9857a
이정훈 <eu4ng97@gmail.com>
2025-04-20 17:39:12 +0900

    Chore: 플러그인 초기 설정 (#1)
    * Chore: 플러그인 설정 변경
    
    * Chore: WidgetManager 모듈 생성
    
    * Chore: 로그 파일 추가

---
+++ Source/WidgetManager/Private/WidgetManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "WidgetManager.h"
+
+#define LOCTEXT_NAMESPACE "FWidgetManagerModule"
+
+void FWidgetManagerModule::StartupModule()
+{
+    
+}
+
+void FWidgetManagerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWidgetManagerModule, WidgetManager)
---
+++ Source/WidgetManager/WidgetManager.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class WidgetManager : ModuleRules
+{
+    public WidgetManager(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UniversalUtilities",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
