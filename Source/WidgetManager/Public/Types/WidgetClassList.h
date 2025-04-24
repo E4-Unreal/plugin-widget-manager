3c46599e5bc1d5918c6ff88c2f9b49f2175ee9d7
이정훈 <eu4ng97@gmail.com>
2025-04-25 00:50:04 +0900

    Feat: WidgetClassList 구조체 작성

---
+++ Source/WidgetManager/Public/Types/WidgetClassList.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetClassList.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FWidgetClassList
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<TSubclassOf<UUserWidget>> List;
+};
