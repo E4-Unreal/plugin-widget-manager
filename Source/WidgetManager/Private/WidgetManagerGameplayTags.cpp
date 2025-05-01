0570b79b985918490b331e18ca6659fc5bf09d2a
이정훈 <eu4ng97@gmail.com>
2025-05-01 20:25:49 +0900

    Feat: WidgetManagerGameplayTags 파일 작성

---
+++ Source/WidgetManager/Private/WidgetManagerGameplayTags.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WidgetManagerGameplayTags.h"
+
+namespace WidgetManagerGameplayTags
+{
+    namespace UI
+    {
+        namespace Layer
+        {
+            UE_DEFINE_GAMEPLAY_TAG(RootTag, "UI.Layer")
+            UE_DEFINE_GAMEPLAY_TAG(DefaultTag, "UI.Layer.Default")
+        }
+    }
+}
---
+++ Source/WidgetManager/Public/WidgetManagerGameplayTags.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "NativeGameplayTags.h"
+
+namespace WidgetManagerGameplayTags
+{
+    namespace UI
+    {
+        namespace Layer
+        {
+            WIDGETMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(RootTag)
+            WIDGETMANAGER_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(DefaultTag)
+        }
+    }
+}
