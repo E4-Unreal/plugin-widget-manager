6d9985b02e92d6888b5ad7f1315ab8b3cb8715c1
이정훈 <eu4ng97@gmail.com>
2025-05-02 08:12:16 +0900

    Feat: ActivatableLayoutWidget 클래스 작성

---
+++ Source/WidgetManager/Private/Widgets/ActivatableLayoutWidget.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Widgets/ActivatableLayoutWidget.h"
+
+#include "WidgetManagerGameplayTags.h"
+#include "Widgets/CommonActivatableWidgetContainer.h"
+
+using namespace WidgetManagerGameplayTags::UI;
+
+void UActivatableLayoutWidget::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    RegisterLayer(Layer::DefaultTag, DefaultLayer);
+}
+
+void UActivatableLayoutWidget::AddWidget(TSubclassOf<UCommonActivatableWidget> ActivatableWidget, const FGameplayTag& LayerTag)
+{
+    if (!ActivatableWidget) return;
+
+    auto Layer = GetLayer(LayerTag);
+    Layer->AddWidget(ActivatableWidget);
+}
+
+void UActivatableLayoutWidget::RegisterLayer(const FGameplayTag& LayerTag,
+                                             UCommonActivatableWidgetContainerBase* Container)
+{
+    if (bool bCanRegister = LayerTag.IsValid() && !LayerMap.Contains(LayerTag) && Container; !bCanRegister) return;
+
+    LayerMap.Emplace(LayerTag, Container);
+}
---
+++ Source/WidgetManager/Public/Widgets/ActivatableLayoutWidget.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "Types/GameplayLayerTag.h"
+#include "ActivatableLayoutWidget.generated.h"
+
+class UCommonActivatableWidgetContainerBase;
+
+/**
+ *
+ */
+UCLASS()
+class WIDGETMANAGER_API UActivatableLayoutWidget : public UCommonActivatableWidget
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "State")
+    TMap<FGameplayTag, TObjectPtr<UCommonActivatableWidgetContainerBase>> LayerMap;
+
+private:
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = true))
+    TObjectPtr<UCommonActivatableWidgetContainerBase> DefaultLayer;
+
+public:
+    virtual void NativePreConstruct() override;
+
+    UFUNCTION(BlueprintCallable)
+    virtual void AddWidget(TSubclassOf<UCommonActivatableWidget> ActivatableWidget, UPARAM(meta = (Categories = "UI.Layer")) const FGameplayTag& LayerTag = FGameplayTag());
+
+    FORCEINLINE UCommonActivatableWidgetContainerBase* GetDefaultLayer() { return DefaultLayer; }
+
+protected:
+    UFUNCTION(BlueprintCallable)
+    virtual void RegisterLayer(UPARAM(meta = (Categories = "UI.Layer")) const FGameplayTag& LayerTag, UCommonActivatableWidgetContainerBase* Container);
+
+    UFUNCTION(BlueprintPure)
+    UCommonActivatableWidgetContainerBase* GetLayer(UPARAM(meta = (Categories = "UI.Layer")) const FGameplayTag& LayerTag) const { return LayerMap.Contains(LayerTag) ? LayerMap[LayerTag] : DefaultLayer; }
+};
